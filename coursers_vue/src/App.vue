29a3dd4bf07750b0d5a6f87e8bac5f95d0c1d8f3
Bloomberg2000 <pxfwxhpb@live.com>
2019-08-25 12:35:01 +0800

    Update

---
+++ coursers_vue/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+    <a-layout id="app" style="min-height: 100vh">
+        <a-layout-sider collapsible v-model="collapsed">
+            <!--            <div class="logo"/>-->
+            <div class="logo">
+                <img style="height: 32px" :src="imgpath.public.logo"/>
+                <span class="logoText" v-show="!collapsed">课程共享平台</span>
+            </div>
+            <a-menu theme="dark" :defaultSelectedKeys="['1']" mode="inline">
+                <a-menu-item key="1">
+                    <a-icon type="schedule"/>
+                    <span>选课系统</span>
+                </a-menu-item>
+                <a-menu-item key="2">
+                    <a-icon type="team"/>
+                    <span>友情链接</span>
+                </a-menu-item>
+                <a-sub-menu key="sub1">
+                    <span slot="title"><a-icon type="user"/><span>个人中心</span></span>
+                    <a-menu-item key="3">我的信息</a-menu-item>
+                    <a-menu-item key="4">选课记录</a-menu-item>
+                </a-sub-menu>
+            </a-menu>
+        </a-layout-sider>
+        <a-layout>
+            <a-layout-header style="background: #fff; padding: 0"/>
+            <a-layout-content style="margin: 0 16px">
+                <router-view/>
+            </a-layout-content>
+            <a-layout-footer style="text-align: center">
+                课程共享平台 ©2018
+            </a-layout-footer>
+        </a-layout>
+    </a-layout>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                collapsed: false,
+            }
+        },
+        computed: {
+            imgpath() {
+                return this.$store.state.imageStyle;
+            },
+        }
+    }
+</script>
+
+<style>
+    #app .logo {
+        height: 32px;
+        margin: 21px;
+    }
+
+    .logo .logoText {
+        font-size: 15px;
+        color: #ffffff;
+        padding: 10px;
+
+    }
+</style>
---
+++ coursers_vue/src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="home">
+        <a-breadcrumb style="margin: 16px 0">
+            <a-breadcrumb-item>选课系统</a-breadcrumb-item>
+        </a-breadcrumb>
+        <div :style="{ padding: '24px', background: '#fff', minHeight: '450px' }">
+            <a-table :columns="columns" :dataSource="data" :scroll="{ y: 400 }">
+                <div slot="detail" slot-scope="text">
+                    <a-button size="default">查看详情</a-button>
+                    <a-divider type="vertical"></a-divider>
+                    <a-button type="primary" size="default">我要选课</a-button>
+                </div>
+            </a-table>
+        </div>
+    </div>
+</template>
+
+<script>
+    const columns = [{
+        title: 'Name',
+        dataIndex: 'name',
+        width: 150,
+    }, {
+        title: 'Age',
+        dataIndex: 'age',
+        width: 150,
+    }, {
+        title: 'Address',
+        dataIndex: 'address',
+    }, {
+        title: 'Action',
+        key: 'operation',
+        fixed: 'right',
+        width: 300,
+        scopedSlots: {customRender: 'detail'},
+    },];
+
+    const data = [];
+    for (let i = 0; i < 100; i++) {
+        data.push({
+            key: i,
+            name: `Edward King ${i}`,
+            age: 32,
+            address: `London, Park Lane no. ${i}`,
+        });
+    }
+
+    export default {
+        data() {
+            return {
+                data,
+                columns,
+            }
+        }
+    }
+</script>
